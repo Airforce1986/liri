f87fc85c59de496baaa468316307e5fac2c0c0af
wikiwoo <wikiwoo@wikiwoos-MacBook-Air.local>
2018-03-01 11:15:02 -0600

    liri

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('Keys is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '2ZUV3ksMFIHV5rzuFqlQ5bCji',
+  consumer_secret: 'hU7AvPNr3fvOkKmOzqqZnLiPTcBsdlqOZAvZJXXQfqNtcORwXm',
+  access_token_key: '814521148159512576-vXEuVqiMyaoOYXXjsFlfl8aK66fJch3',
+  access_token_secret: 'qRYE3Z8HmQEsCCN8iTksNrHLdh7qhh570uGGYpKWEmorK',
+}
+
+
